cec40f792ee3f710d665b4f22ccdda05e8fb91f3
Painless <82113943+PainlessB3n@users.noreply.github.com>
2024-11-24 14:59:01 -0500

    Removed unused code

--- src/modules/combos.py
+++ src/modules/combos.py
@@ -1,22 +1,6 @@
 from modules.patterns import PATTERNS
 import random
 
-# def comboWeights():
-#     # Stage: [Probability of Difficulty N Combo, ...]
-#     return [
-#         [0.7, 0.3, 0], # Stage 1
-#         [0.3, 0.5, 0.2], # Stage 2
-#         [0, 0.3, 0.7]  # Stage 3
-#     ]
-
-# def loadCombos():
-#     # Difficulty N Combos
-#     return [
-#         [], # 0000000 1 Combos
-#         [], # Difficulty 2 Combos
-#         []  # Difficulty 3 Combos
-#     ]
-
 def getRandomPatterns(amount, replacement=False):
     if replacement:
         return random.choices(list(PATTERNS.keys()), k=amount)
@@ -25,12 +9,3 @@ def getRandomPatterns(amount, replacement=False):
 
 def combo():
     return getRandomPatterns(3, replacement=False)
-
-# class RandomCombo(Combo):
-#     def __init__(self, size):
-        
-
-
-# class RepeatingCombo(Combo):
-#     def __init__():
-#         pass
