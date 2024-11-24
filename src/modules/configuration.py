8cd648b37c1908f442d70733a9901bc6f53279ae
Painless <82113943+PainlessB3n@users.noreply.github.com>
2024-11-24 14:59:28 -0500

    Update configuration.py

--- src/modules/configuration.py
+++ src/modules/configuration.py
@@ -3,5 +3,5 @@ CONFIGURATION = {
     'startingLives': 1,
     'enemyVelocity': 100, # Pixels per second
     'enemySpawnDelay': 3, # Second inbetween enemies spawning
-    'scorePerEnemyKilled': 50 # Second inbetween enemies spawning
+    'scorePerEnemyKilled': 50
 }
