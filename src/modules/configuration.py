d6e21dc3b174f8605185b5a3ee56de598785e566
Painless <82113943+PainlessB3n@users.noreply.github.com>
2024-11-24 15:04:48 -0500

    Decreased enemy speed

--- src/modules/configuration.py
+++ src/modules/configuration.py
@@ -1,7 +1,7 @@
 CONFIGURATION = {
     'deltaSegmentTolerance': 15,
     'startingLives': 1,
-    'enemyVelocity': 100, # Pixels per second
+    'enemyVelocity': 60, # Pixels per second
     'enemySpawnDelay': 3, # Second inbetween enemies spawning
     'scorePerEnemyKilled': 50
 }
